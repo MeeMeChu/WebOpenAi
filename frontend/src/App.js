bfa0e6f29939f3c9873182a80e2ec8f6ecf5cbca
MWit Mac Mini2 <mwitmacmini2@MWits-Mac-mini.local>
2023-07-11 20:37:39 +0700

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Header from "./component/Header"
+
+import { Button } from "antd"
+
+const App = () => {
+
+
+    return (
+        <>
+            <Header />
+            <Button type="primary">test</Button>
+        </>
+    )
+}
+
+export default App
---
+++ frontend/src/component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "antd"
+
+const Header = () => {
+    return (
+        <>
+            <Button type="dashed">test1</Button>
+        </>
+    )
+}
+
+export default Header
