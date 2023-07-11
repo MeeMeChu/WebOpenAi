3501447faef5504a8828768c205af7a1ede6b2e5
MWit Mac Mini 1 <csmwit1@MWits-Mac-mini.local>
2023-07-11 19:53:13 +0700

    lern API

--- Backend API/main.py
+++ Backend API/main.py
@@ -1,8 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get("/")
@@ -11,5 +17,19 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_price": item.price , "item_id": item_id}
+    
+
+@app.post("/items/{item_id}")
+def edit_item(item_id: int, item: Item):
+    return {"item_name": item.name, "itema_price": item.price}
+
+@app.delete("/items/{items_id}")
+def del_item(item_id: int):
+    return {"Delete Complete!!"}
